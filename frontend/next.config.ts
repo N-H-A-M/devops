e56c44cee7f5aa71b2daec2c16cd486e3e922477
N-H-A-M <nielhaim@gmail.com>
2026-07-30 20:39:49 +0300

    updates k8s files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  allowedDevOrigins: [
-      '127.0.0.1',
-      'localhost',
-  ],
-  };
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
